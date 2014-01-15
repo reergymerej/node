d726820a8c4ca122a782c0d20c34e725d3d416b9
Jeremy Greer <jeremygreer@reergymerej.com>
2014-01-15 07:28:51 -0500

    init commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.helloworld = function (req, res) {
+  res.render('helloview', {
+    title: 'world'
+  });
+};
+
+exports.userlist = function(db) {
+    return function(req, res) {
+        var collection = db.get('usercollection');
+        collection.find({},{},function(e,docs){
+            res.render('userlist', {
+                "userlist" : docs
+            });
+        });
+    };
+};
